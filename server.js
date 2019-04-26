0c031a483d5312bfba6b7170cad271c037e0c2e6
vaibhav <karnam.v@husky.neu.edu>
2019-04-25 21:34:37 -0400

    Changing CORS header for hosted UI

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
-    res.header("Access-Control-Allow-Origin",
         "https://easy-payroll-app.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
