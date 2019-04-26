00754acfb12ce98a9020bb156dc0dc1d5f9760f2
vaibhav <karnam.v@husky.neu.edu>
2019-04-25 21:30:15 -0400

    Revert "Adding CORS header for new host"
    This reverts commit 612086cef1a142703aa658293a3a3881a4ff90f3.

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
         "http://localhost:4200");
-    res.header("Access-Control-Allow-Origin",
-        "https://easy-payroll-app.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
